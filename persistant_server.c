efdd731c1dc80488000beef7ff183ad94d5a8043
Christophe Casino <ccasino50@stuy.edu>
2025-01-07 20:56:04 -0500

    added nested while loops for more than one client handling

--- persistant_server.c
+++ persistant_server.c
@@ -1,11 +1,38 @@
 #include "pipe_networking.h"
 
+static void sighandler(int signo){
+  if(signo == SIGINT){
+    char path[] = "/tmp/mario";
+    remove(path);
+    exit(0);
+  }
+}
+
 int main() {
-  
-  int to_client;
-  int from_client;
+  signal(SIGINT, sighandler);
+  srand(time(NULL));
+  int to_client; // Private
+  int from_client; // WKP
+  int WKPfd;
 
   to_client = server_setup();
+  WKPfd = to_client;
 
-  from_client = server_handshake( &to_client );
+  while(1){
+    from_client = server_handshake( &to_client );
+
+    while(1){
+      int random = (rand() % 101);
+      char sent_int[20];
+      sprintf(sent_int, "%d", random);
+      int n = write(to_client, sent_int, sizeof(sent_int));
+      if(n < 0){
+        close(to_client);
+        to_client = WKPfd;
+        printf("SERVER BREAK\n");
+        break;
+      }
+      sleep(1);
+    }
+  }
 }
