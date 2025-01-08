e4419f9fb43aa6554dd62a8f3a688a6cdffa2491
Christophe Casino <ccasino50@stuy.edu>
2025-01-07 19:36:33 -0500

    trying to figure out how to communicate to client from basic server file

--- basic_server.c
+++ basic_server.c
@@ -1,11 +1,18 @@
 #include "pipe_networking.h"
 
 int main() {
-  
+  srand(time(NULL));
   int to_client;
   int from_client;
 
   to_client = server_setup();
 
   from_client = server_handshake( &to_client );
+  while(1){
+    int random = (rand() % 101);
+    char path_prefix[100] = "/tmp/";
+    char private_path[100];
+    sprintf(private_path, "%s%d", path_prefix, *to_client);
+    sleep(1);
+  }
 }
