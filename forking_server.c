f4d3e85f1bba9ac9e3df52ba0e41e6c06a246b3a
Christophe Casino <ccasino50@stuy.edu>
2025-01-08 16:10:26 -0500

    added forking server file with a copy pasted persistant server code

---
+++ forking_server.c
@@ -0,0 +1,40 @@
+#include "pipe_networking.h"
+
+static void sighandler(int signo){
+  if(signo == SIGINT){
+    char path[] = "/tmp/mario";
+    remove(path);
+    exit(0);
+  }
+  if(signo == SIGPIPE){
+    printf("\n");
+  }
+}
+
+int main() {
+  signal(SIGINT, sighandler);
+  signal(SIGPIPE, sighandler);
+  srand(time(NULL));
+  int to_client; // Private
+  int from_client; // WKP
+  int WKPfd;
+
+  while(1){
+    to_client = server_setup();
+    WKPfd = to_client;
+    from_client = server_handshake( &to_client );
+
+    while(1){
+      int random = (rand() % 101);
+      char sent_int[20];
+      sprintf(sent_int, "%d", random);
+      int n = write(to_client, sent_int, sizeof(sent_int));
+      if(n < 0){
+        close(to_client);
+        to_client = WKPfd;
+        break;
+      }
+      sleep(1);
+    }
+  }
+}
