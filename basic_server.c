ed6bbcfda66cd4eb8ccb7cae47c26df6b1c76380
Christophe Casino <ccasino50@stuy.edu>
2025-01-07 19:40:25 -0500

    able to send int through the private pipe

--- basic_server.c
+++ basic_server.c
@@ -8,11 +8,19 @@ int main() {
   to_client = server_setup();
 
   from_client = server_handshake( &to_client );
+  char path_prefix[100] = "/tmp/";
+  char private_path[100];
+  sprintf(private_path, "%s%d", path_prefix, *to_client);
+  int fd = open(private_path, O_WRONLY);
+  if(fd == -1) {
+    printf("%s\n",strerror(errno));
+    exit(1);
+  }
   while(1){
     int random = (rand() % 101);
-    char path_prefix[100] = "/tmp/";
-    char private_path[100];
-    sprintf(private_path, "%s%d", path_prefix, *to_client);
+    char sent_int[20];
+    sprintf(sent_int, "%d", random);
+    write(fd, sent_int, sizeof(sent_int));
     sleep(1);
   }
 }
